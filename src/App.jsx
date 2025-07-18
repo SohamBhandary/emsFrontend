00ba9624329c6e97ce717224a5ef2f79151254b4
SohamBhandary <bhandarysoham27@gmail.com>
2025-07-18 19:10:57 +0530

    header footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ListEmployeeComponents from './components/ListEmployeeComponents'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+import "./App.css"
 
 const App = () => {
   return (
    <>
+   <HeaderComponent/>
   <ListEmployeeComponents/>
+  <FooterComponent/>
 
    </>
   )
